bceaeca7397080be512d6e82c29e24541080efbe
George Goodman <georgegoodman@Georges-MacBook-Pro.local>
2019-08-06 18:05:21 -0400

    Starting Commit

---
+++ Assets/Scripts/ComponentScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentScript : MonoBehaviour
+{
+
+    public int componentOptionCount = 6;
+    public string[] componentOptions;
+    public string componentName;
+    public Sprite componentSprite;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
